4f2e037708f19882c8f078d9b3c04f31cb09ed68
Plasmatium <slv309@163.com>
2016-04-19 23:46:26 +0800

    test ok

--- util.py
+++ util.py
@@ -1,7 +1,10 @@
+#!/usr/bin/env python3.5
+
 from PIL import Image,ImageFont,ImageDraw
 import numpy as np
+import pandas as pd
 
-font = ImageFont.truetype("msyh.ttf",32)
+font = ImageFont.truetype("wqy-zenhei.ttc",32)
 
 #convert word to utf-8 encoding, returned as a tuple
 def w2u(u):
@@ -20,6 +23,45 @@ def get_grey_scale(word):
 	return np.array(im).mean()
 
 def get_gs_dict():
-	for u in range(0x4e00,0x9fb0):
-		txt = bytearray(w2u(u)).decode()
-		#todo: add
+	dict_list = []
+	for u in range(0x20,0xff):
+		try:
+			txt = chr(u)
+		except:
+			print('error on decode %u'%u)
+			continue
+		grey = get_grey_scale(txt)
+		dict_list.append((u, (grey, txt)))
+
+		#progress bar
+		if u%0x10==0:
+			print('%f%%'%( (u-0x0000)/(0xff)*100 ))
+	rslt = pd.DataFrame(dict(dict_list)).T
+	return rslt
+
+def nearest(n, li):
+	diff = abs(n-li).min()
+	if n+diff in li.values:
+		return n+diff
+	else:
+		return n-diff
+
+img = Image.open('test.jpg').convert('L')
+img_arr = np.array(img.resize((250, 163)))
+
+dct = get_gs_dict()
+dct[0] -= dct[0].min()
+m = dct[0].max()
+r = 255/m
+dct[0] *= r
+
+li = dct[0]
+
+a=''
+idx=0
+for arr in img_arr:
+	a += ''.join([dct[dct[0]==nearest(x, li)][1].values[0] for x in arr])+'\n'
+	print(idx)
+	idx += 1
+with open('img.txt', 'w') as f:
+	f.write(a)
